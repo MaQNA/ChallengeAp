5377cad0daf397f39f2b0977d618334396825136
MaQNA <magdalena.k.kuna@gmail.com>
2023-07-24 10:11:32 +0200

    Zadanie domowe dzień 4

---
+++ ChallengeApp/ChallengeApp/Program1.cs
@@ -0,0 +1,37 @@
+﻿/// ZADANIE DOMOWE (DZIEŃ 4)
+
+string name = "Zosia";
+char sex = 'F';
+int age = 28;
+
+if (sex == 'F')
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    
+}
+else if (name == "Ewa" && age == 33) 
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == 'M' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
+
+
+
+bool textIsOK = true;
+
+if (textIsOK == true)
+{
+    Console.WriteLine("MAM TĘ MOC");
+}
+
+else if (textIsOK == false)
+{
+    Console.WriteLine("MOCY BRAK");
+}
